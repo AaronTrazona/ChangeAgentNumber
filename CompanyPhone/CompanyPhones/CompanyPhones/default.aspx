6b52f6567624bc13cfdd629f54f92b865c4e1758
Aaron Trazona <dna.trazona@gmail.com>
2012-04-17 01:24:11 -0700

    ChangeAgentNumber

---
+++ CompanyPhone/CompanyPhones/CompanyPhones/default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="CompanyPhones._default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div align ="center" style="height: 67px" >
+    <br/>
+    <asp:Label Text = "Select Company" runat = "server"></asp:Label>
+     <br/> <br />
+    <asp:DropDownList ID="dropCompany" runat="server" Height="25px" Width="225px" 
+            OnSelectedIndexChanged = "Selected_Change" 
+            ontextchanged="dropCompany_TextChanged" AutoPostBack = "true">
+    </asp:DropDownList>
+    <br />
+    <br />
+    <asp:Label ID="Label2" Text = "Select Company Agent" runat = "server" Visible = "false"></asp:Label>
+    <br/> <br />
+    <asp:DropDownList ID="dropAgent" runat="server" Height="25px" Width="225px" OnSelectedIndexChanged = "Selected_ChangeInAgent"
+    AutoPostBack = "true">
+    </asp:DropDownList>
+    <br />
+    <br />
+    <asp:Label ID="Label3" Text = "Change Agent PhoneNumber" runat = "server" Visible = "false"></asp:Label>
+    <br />
+    
+        <asp:Label ID="Label4" runat="server" Text="" Visible = "false"></asp:Label>
+    <br />
+    <br />
+    <asp:TextBox ID="txtPhone" runat="server" Width="177px" Visible = "false"></asp:TextBox>
+    <br />
+     <asp:Button ID="btnChange" runat="server" Height="24px" Text="Updated"  onclick="btnUpdate_Click"/>
+    </div>
+       
+    </form>
+
+</body>
+</html>
---
+++ CompanyPhone/CompanyPhones/CompanyPhones/default.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CompanyPhones
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        public DrumbiDataContext db = new DrumbiDataContext();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+            if (dropCompany.SelectedValue == "" )
+            {
+                int count;
+                count = 1;
+                List<tbl_Company> companies = (from c in db.tbl_Companies
+                                               select c).ToList();
+                ListItem[] items = new ListItem[companies.Count() +1];
+                items[0] = new ListItem("", "0");
+                
+                companies.ForEach(delegate(tbl_Company company)
+                {
+                   
+                    items[count] = new ListItem(company.CompanyName, company.CompanyId.ToString());
+                    count++;
+                    
+                });
+
+                dropCompany.Items.AddRange(items);
+                dropCompany.DataBind();
+                
+            }
+        }
+
+        protected void Selected_Change(object sender, EventArgs e)
+        {
+            dropAgent.Items.Clear();
+            int count;
+            Label2.Visible = true;
+            count = 1;
+            List<tbl_CompanyAgent> agents = (from a in db.tbl_CompanyAgents
+                                             where a.CompanyID == int.Parse(dropCompany.SelectedValue)
+                                             select a).ToList();
+
+           
+            ListItem[] items = new ListItem[agents.Count() + 1];
+            items[0] = new ListItem("", "0");
+            agents.ForEach(delegate(tbl_CompanyAgent agent)
+            {
+                items[count] = new ListItem(agent.FirstName, agent.AgentID.ToString());
+
+                count++;
+            });
+
+            dropAgent.Items.AddRange(items);
+            dropAgent.DataBind();
+            txtPhone.Visible = false;
+            Label3.Visible = false;
+            Label4.Visible = false;
+           
+        }
+
+        protected void Selected_ChangeInAgent(object sender, EventArgs e)
+        {
+
+             Label4.Text = "Current Phone Number: " + db.tbl_CompanyAgents.SingleOrDefault(a => a.AgentID == int.Parse(dropAgent.Text)).Phone;
+
+             if (Label4.Text != "Current Phone Number: ")
+             {
+                 txtPhone.Visible = true;
+                 Label3.Visible = true;
+                 Label4.Visible = true;
+             }
+             else
+             {
+
+                 txtPhone.Visible = false;
+                 Label3.Visible = false;
+                 Label4.Visible = false;
+
+             }
+            
+            
+        }
+
+        protected void dropCompany_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int agentID;
+
+           
+
+            try
+            {
+                if (!string.IsNullOrEmpty(dropAgent.SelectedValue) && !string.IsNullOrEmpty(txtPhone.Text))
+                {
+
+                    int.TryParse(dropAgent.SelectedValue, out agentID);
+                    var agent = db.sp_ChangeAgentPhoneNumber((int)agentID, txtPhone.Text);
+
+
+                    if (agent.ReturnValue.ToString() == "0")
+                        MessageBox("Error in Changing Agent Phone");
+                    else
+                        Response.Redirect(Request.RawUrl);
+
+                }
+                else
+                {
+                    MessageBox("Fill In All Fields.");
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox("Error in Changing Agent Phone");
+            }
+
+        
+        }
+
+
+        private void MessageBox(string message)
+        {
+
+            string tmp = "";
+            tmp = "<script language='javascript'>";
+            tmp += "alert('" + message + "');";
+            tmp += "</script>";
+            Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "MyScript", tmp);
+
+        }
+
+        
+    }
+}
